2d2a4b0bf6829d7eb896bbce81d29d7a713d8a06
Nikson Colhado <nmcolhado@FUGINI.COM>
2022-10-06 15:30:30 -0300

    JavaScript

---
+++ Aulas-JavaScript-GustavoGuanabara/005-Arrays.js
@@ -0,0 +1,19 @@
+let num = [8, 13, 4, 1, 3, 0, 9 , 7];
+
+//num.push(34);
+let tamanho = num.length;
+//num.sort();
+
+console.log(`O vetor tem tamanho de ${tamanho} e os seguintes elementos: ${num} \n`);
+
+for(let pos in num){ //para cada posição dentro do NUM
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`);
+}
+
+let encontrar = num.indexOf(3);
+
+if(encontrar == -1){
+    console.log("Valor não foi encontrado!");
+}else{
+    console.log(`Valor encontrado no índice ${encontrar}`);
+}
